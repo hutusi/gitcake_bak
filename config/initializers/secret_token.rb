1916d6967d5fcfb3b9793a29fc32de2db619c0f6
John Hu <huziyong@gmail.com>
2014-02-15 22:20:48 +0800

    New rails app without test unit framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitcake::Application.config.secret_key_base = '5be8db35c6f53c7bc71dffb6c8f36319774528829755412c2546b5987d05f0bcf2fa6d6819361d3c10ad254293ef65605bea72e3a88c96cd99907607dfe69ca3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gitcake::Application.config.session_store :cookie_store, key: '_gitcake_session'
